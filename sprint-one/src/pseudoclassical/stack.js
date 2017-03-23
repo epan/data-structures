cea9729c4028c0e08629e1a7721689ab80e1bd06
MKS-Student <mks.student@hackreactor.com>
2017-03-23 15:48:13 -0700

    Pass pseudoclassical tests for stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.index = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
 
+Stack.prototype.pop = function() {
+  var output = this.storage[this.index - 1];
+  delete this.storage[this.index - 1];
+  this.index--;
+  return output;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
