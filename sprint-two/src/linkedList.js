407007fdfacc4e407cfcd4cf703e861f565595bc
MKS-Student <mks.student@hackreactor.com>
2017-03-23 18:55:56 -0700

    Add to tail and remove from head
    Need to work on: .contains()

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,11 +2,30 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.storage=[];
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.storage.length > 0) {
+      list.storage[list.storage.length - 1].next = newNode.value;
+    } 
+    list.storage.push(newNode);
+    list.tail = newNode; 
+    if (list.storage.length === 1){
+      list.head = newNode; 
+    }
+
   };
 
   list.removeHead = function() {
+    var output = list.storage.shift();
+    if (list.storage.length === 0) {
+      list.head = null;
+      list.tail = null;
+    } else {
+      list.head = list.storage[0];
+    }
+    return output.value;
   };
 
   list.contains = function(target) {
@@ -27,3 +46,23 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// var newNode = {'value': 4, 'next': null}
+
+
+
+// var list {
+//   'tail': null
+// }
+
+// var list {
+//   'tail': {
+//     'next': 4
+//   }
+// }
+
+// var list {
+//   'tail': {
+//     'value': 4
+//     'next': null
+//   }
+// }
