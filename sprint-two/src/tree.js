56819026bc1dd15a286afce205398ec08ec902f8
MKS-Student <mks.student@hackreactor.com>
2017-03-24 11:02:55 -0700

    Remove pseudocode and comments

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,7 +2,6 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
   newTree.children = [];
   _.extend(newTree, treeMethods);
 
@@ -12,16 +11,10 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // push value to this.children
   this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
-  // inner search function with 'this'
-  // if target === this.value
-    // return true
-  // else call search with this.children
-  // return false;
   var output = false;
   if (target === this.value) {
     return true;
@@ -29,6 +22,7 @@ treeMethods.contains = function(target) {
   for (var i = 0; i < this.children.length; i++) {
     output = output || this.children[i].contains(target);
   }
+
   return output;
 };
 
