bc27ac3e035ab6a073c94c984654c119807dcc93
MKS-Student <mks.student@hackreactor.com>
2017-03-25 09:44:49 -0700

    Add methods and complexity estimates

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,33 @@
 var BinarySearchTree = function(value) {
+
+  var binTree = {};
+  binTree.value = value;
+  binTree.left = undefined;
+  binTree.right = undefined;
+
+  _.extend(binTree, binTreeMethods);
+
+  return binTree;
+};
+
+var binTreeMethods = {};
+
+binTreeMethods.insert = function() {
+
+};
+
+binTreeMethods.contains = function() {
+
+};
+
+binTreeMethods.depthFirstLog = function() {
+
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ // .insert: Log O(logn), worst case Linear O(n)
+ // .contains: Log O(logn), worst case Linear O(n)
+ // .depth: Linear O(n)
