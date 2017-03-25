1d2178da11786e85dc0748a1c5bbd149adf0dcb1
MKS-Student <mks.student@hackreactor.com>
2017-03-25 10:38:53 -0700

    Pass insert test for binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -12,8 +12,39 @@ var BinarySearchTree = function(value) {
 
 var binTreeMethods = {};
 
-binTreeMethods.insert = function() {
+binTreeMethods.insert = function(value, currNode) {
+  var newNode = BinarySearchTree(value);
+  if (currNode === undefined) {
+    currNode = this;
+  }
 
+  if (value < currNode.value) {
+    if (currNode.left === undefined) {
+      currNode.left = newNode;
+    } else {
+      currNode.left.insert(value, currNode.left)
+    }
+  } else if (value > currNode.value) {
+    if (currNode.right === undefined) {
+      currNode.right = newNode;
+    } else {
+      currNode.right.insert(value, currNode.right)
+    }
+  }
+
+  // start at current node (this)
+  // if input value is less than current node
+    // go to left child node
+    // if left child node is undefined
+      // save the new node here
+    // else recurse with left child node as current node
+  // else if input value is greater than current node
+    // go to right child node
+    // if right child node is undefined
+      // save the new node here
+    // else recurse with right child node as current node
+
+// call .insert with highest this.value;
 };
 
 binTreeMethods.contains = function() {
