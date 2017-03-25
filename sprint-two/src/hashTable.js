db48cc7396c2a02dae7af29d84ce85d4629063b4
MKS-Student <mks.student@hackreactor.com>
2017-03-24 21:20:43 -0700

    pass HashTable insert and retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,40 @@
 
 
 var HashTable = function() {
+
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
+  for (var i = 0; i < this._limit; i++) {
+    this._storage[i] = [];
+  }
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var oldBucket = this._storage[index];
+  oldBucket.push([k, v]);
+  this._storage.set(index, oldBucket);
+    // check if k is already present
+      //if no,
+      //then push [k,v] to bucket
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var result = '';
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      result = bucket[i][1];
+    }
+  }
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //for each
 };
 
 
@@ -22,5 +42,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
